47c12ca00a31f343b13b99f76f00f8d34ddca3ba
SvitlanaVelychko <svel2608@gmail.com>
2022-08-12 01:10:32 +0300

    Solve task

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -14,6 +14,7 @@ export const FeedbackBtn = styled.button`
     width: 100px;
     height: 40px;
     cursor: pointer;
+    font-weight: ${p => p.theme.fontWeights.bold};
     border: ${p => p.theme.borders.normal};
     border-radius: ${p => p.theme.radii.normal};
     transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
